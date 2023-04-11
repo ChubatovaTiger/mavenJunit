73cce720d08fec641fb0b577b6dc816685422664
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-04-11 17:31:33 +0200

    Update MavenDemoApplication4Tests.java

--- src/test/java/com/jetbrains/mavendemo/MavenDemoApplication4Tests.java
+++ src/test/java/com/jetbrains/mavendemo/MavenDemoApplication4Tests.java
@@ -3,7 +3,6 @@ package com.jetbrains.mavendemo;
 //import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.assertj.core.api.Assertions.assertThat;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +10,26 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import System;
+
+private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+private final ByteArrayOutputStream err = new ByteArrayOutputStream();
+private final PrintStream originalOut = System.out;
+private final PrintStream originalErr = System.err;
+
+@Before
+public void setStreams() {
+    System.setOut(new PrintStream(out));
+    System.setErr(new PrintStream(err));
+}
+
+@After
+public void restoreInitialStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
+}
+
+
 
 @SpringBootTest
 class MavenDemoApplication4Tests {
@@ -25,6 +44,7 @@ class MavenDemoApplication4Tests {
             {
             // this part is executed when an exception (in this example InterruptedException) occurs
             }
+		System.out.print("test output");
 		assertThat("foo1").isEqualTo("foo1");
 
 	}
