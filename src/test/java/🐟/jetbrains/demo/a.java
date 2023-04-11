c07086098c7086ef25cc41dce456def075b7e6ca
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-04-11 17:13:11 +0200

    Create a.java

---
+++ src/test/java/🐟/jetbrains/demo/a.java
@@ -0,0 +1,42 @@
+package 🐟.jetbrains.mavendemo;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.assertj.core.api.Assertions.assertThat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+ import java.lang.Thread;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class a {
+
+	@Test
+	void contextLoads11() {
+		try 
+            {
+                Thread.sleep(60000);
+            } 
+            catch(InterruptedException e)
+            {
+            // this part is executed when an exception (in this example InterruptedException) occurs
+            }
+		assertThat("foo1").isEqualTo("foo1");
+
+	}
+		@Test
+	void contextLoads32() {
+		assertThat("foo").isEqualTo("foo");
+
+	}
+		@Test
+	void contextLoads33() {
+		assertThat("foo").isEqualTo("foo");
+
+	}
+
+}
