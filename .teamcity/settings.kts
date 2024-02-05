05cc07ed7f9856d88570666142df3412699fe24a
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2024-02-05 20:28:23 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -41,7 +41,7 @@ object Kkjkj : BuildType({
         maven {
             goals = "clean test"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
-            mavenVersion = bundled_3_2()
+            mavenVersion = bundled_3_3()
         }
     }
 })
