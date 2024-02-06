8fbf404ff293677c7a9a41f3c8ed7b8d730222bb
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2024-02-06 12:42:37 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,23 +2,17 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
-
 version = "2023.11"
 
 project {
-
-
-
     val numProjects=12
     val numConfigurationsPerProject=800
-
-
     for (i in 0..numProjects) {
         subProject {
             id("subProj_$i")
             name = "subProj $i"
+        for (j in 0..1) {
 
-for (j in 0..1) {
                 buildType {
                     id("Auto_$i" + "_$j")
                     name = "auto $i $j"                    
@@ -50,8 +44,56 @@ for (j in 0..1) {
                             }
                     }
                 }
-}
 
+            subProject {
+                id("subProj2_$i")
+                name = "subProj2 $i"
+                subProject {
+                id("subProj3_$i")
+                name = "subProj3 $i"
+                subProject {
+                id("subProj4_$i")
+                name = "subProj4 $i"
+                subProject {
+                id("subProj5_$i")
+                name = "subProj5 $i"
+                subProject {
+                id("subProj6_$i")
+                name = "subProj6 $i"
+                }
+                subProject {
+                id("subProj7_$i")
+                name = "subProj7 $i"
+                subProject {
+                id("subProj8_$i")
+                name = "subProj8 $i"
+
+                 buildType {
+                    id("BundledDefault363_$i" + "_$j")
+                    name = "BundledDefault363 $i $j"                    
+                    vcs {
+                        root(DslContext.settingsRoot)
+                        }
+                    steps {
+                    maven {
+                    id = "Maven2"
+                    goals = "clean test"
+                    runnerArgs = "-Dmaven.test.failure.ignore=true"
+                    //mavenVersion = bundled_3_2()
+                            }
+                    }
+                }
+
+
+                }
+                }    
+                }
+                }
+                }
+            }
+
+
+            }
         }
     }
 }
