ae88e41f6ff9ed75e6dac68c5cc1484e1bc740c2
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2024-02-06 12:52:29 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -85,6 +85,65 @@ project {
                     }
                 }
 
+                 buildType {
+                    id("NonBundled396_$i" + "_$j")
+                    name = "NonBundled396 $i $j"                    
+                    vcs {
+                        root(DslContext.settingsRoot)
+                        }
+                    steps {
+                    maven {
+                    id = "Maven2"
+                    goals = "clean test"
+                    runnerArgs = "-Dmaven.test.failure.ignore=true"
+                    mavenVersion = custom {
+                    path = "%teamcity.tool.maven.3.9.6%"
+                }
+                            }
+                    }
+                }
+
+                 buildType {
+                    id("Nonbundled322_$i" + "_$j")
+                    name = "Nonbundled322 $i $j"                    
+                    vcs {
+                        root(DslContext.settingsRoot)
+                        }
+                    steps {
+                    maven {
+                    id = "Maven2"
+                    goals = "clean test"
+                    runnerArgs = "-Dmaven.test.failure.ignore=true"
+                    mavenVersion = custom {
+                    path = "%teamcity.tool.maven.3.2.2%"
+                }
+                            }
+                    }
+                }
+
+                 buildType {
+                    id("outsideOfRunner_$i" + "_$j")
+                    name = "outsideOfRunner $i $j"                    
+                    vcs {
+                        root(DslContext.settingsRoot)
+                        }
+                    params {
+                param("mvn2", "%teamcity.tool.maven3_8%")
+                }
+
+                    steps {
+                    script {
+                     id = "simpleRunner"
+                        scriptContent = """
+                        echo '%teamcity.tool.maven3_3%'
+                            echo '%teamcity.tool.maven.3.9.5%'
+                        echo '%mvn2%'
+                        """.trimIndent()
+                    }
+                        }
+                }
+
+
 
                 }
                 }    
@@ -99,131 +158,4 @@ project {
     }
 }
 
-/*
 
-
-project {
-
-    buildType(NonBundled396)
-    buildType(Mvn)
-    buildType(BundledDefault363)
-    buildType(Auto)
-    buildType(Nonbundled322)
-    buildType(OutsideOfRunner)
-
-    params {
-        param("env.M2_HOME", "/opt/homebrew/Cellar/maven/3.9.5/libexec")
-    }
-}
-
-object Auto : BuildType({
-    name = "auto"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            id = "Maven2"
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            mavenVersion = auto()
-        }
-    }
-})
-
-object BundledDefault363 : BuildType({
-    name = "bundledDefault3.6.3"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            id = "Maven2"
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-        }
-    }
-})
-
-object Mvn : BuildType({
-    name = "bundled3.2.5"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            id = "Maven2"
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            mavenVersion = bundled_3_2()
-            dockerImage = "openjdk:17-ea-slim"
-        }
-    }
-})
-
-object NonBundled396 : BuildType({
-    name = "non-bundled3.9.6changed"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            id = "Maven2"
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            mavenVersion = custom {
-                path = "%teamcity.tool.maven.3.9.6%"
-            }
-            dockerImage = "openjdk:17-ea-slim"
-        }
-    }
-})
-
-object Nonbundled322 : BuildType({
-    name = "nonbundled3.2.2"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            id = "Maven2"
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            mavenVersion = custom {
-                path = "%teamcity.tool.maven.3.2.2%"
-            }
-            dockerImage = "openjdk:17-ea-slim"
-        }
-    }
-})
-
-object OutsideOfRunner : BuildType({
-    name = "outsideOfRunner"
-
-    params {
-        param("mvn2", "%teamcity.tool.maven3_8%")
-    }
-
-    steps {
-        script {
-            id = "simpleRunner"
-            scriptContent = """
-                echo '%teamcity.tool.maven3_3%'
-                echo '%teamcity.tool.maven.3.9.5%'
-                echo '%mvn2%'
-            """.trimIndent()
-        }
-    }
-})
-
-*/
